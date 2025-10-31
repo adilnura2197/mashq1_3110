a76557b60b60c48a6e730832cbe6d39c1eb2376c
adilnura2197 <adilnura2197@gmail.com>
2025-10-31 17:40:53 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# 1-misol
+for i in range(1, 11):
+    print(i)
+
+
+# 2-misol
+for i in range(10, 0, -1):
+    print(i)
+
+
+# 3-misol
+n = int(input("Son kirit: "))
+
+for i in range(2, n, 2):
+    print(i)
+
+
+# 4-misol
+n = int(input("Son kirit: "))
+
+for i in range(1, n, 2):
+    print(i)
+
+
+# 5-misol
+n = int(input("Son kirit: "))
+sum = 0
+
+for i in range(1, n):
+    sum += 1
+
+print(sum)
